93330249892c307f80aea890a2594874961a58f1
Kirill Scarlat <163547441+neossakura@users.noreply.github.com>
2024-09-17 21:19:52 +0300

    Add files via upload

---
+++ src/Pages/Technologies.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { techStackDetails } from "../Details";
+
+function Technologies() {
+  const {
+    html,
+    css,
+    js,
+    react,
+    fl,
+    vscode,
+    git,
+    github,
+    figma,
+    python,
+  } = techStackDetails;
+  return (
+    <main className="container mx-auto max-width pt-10 pb-20 ">
+      <section>
+        <h1 className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
+          My Tech Stack
+        </h1>
+        <p className="text-content py-2 lg:max-w-3xl">
+          Technologies I've been working with recently:
+        </p>
+      </section>
+      <section className="grid grid-cols-4 md:grid-cols-5 lg:grid-cols-6 items-center gap-10 pt-6">
+        <img src={html} title="html" alt="" />
+        <img src={css} title="CSS" alt="" />
+        <img src={js} title="JavaScript" alt="" />
+        <img src={react} title="React" alt="" />
+        <img src={fl} title="Fl Studio" alt="" />
+      </section>
+      <section>
+
+      </section>
+      <section className="grid grid-cols-4 md:grid-cols-5 lg:grid-cols-6 items-center gap-10 pt-6">
+        <img src={vscode} title="Visual Studio Code" alt="" />
+        <img src={git} title="Python" alt="Python" />
+        <img src={github} title="Github" alt="Github" />
+        <img src={figma} title="Figma" alt="Figma" />
+      </section>
+    </main>
+  );
+}
+
+export default Technologies;
